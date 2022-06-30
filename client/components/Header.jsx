f0ff9f36035dfdecc26d9801fb43c3df6babf7cf
beyond_wen_mako_22 <103220308+Beyond-Wen@users.noreply.github.com>
2022-06-30 17:38:44 +1200

    images

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header className="header">
+      <img src="" className="header--image" />
       <h1 className="title">Hot Wheels</h1>
     </header>
   )
