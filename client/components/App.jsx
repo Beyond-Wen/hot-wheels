9d1ab4c3d157350c8103524225f60984582a53d5
ayoung-lee <103156367+ayoung-lee@users.noreply.github.com>
2022-06-30 20:13:10 +1200

    time to save

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Header'
 import Car from './Car'
 import AddCar from './AddCar'
+import Card from './Card'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <AddCar />
       <Car />
+      <Card />
     </>
   )
 }
