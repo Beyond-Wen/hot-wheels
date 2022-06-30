515a3beaaf0a71a36999b4c574a4fbe6f4082a0d
Rupert <67182833+rupert-denton@users.noreply.github.com>
2022-06-30 22:31:28 +1200

    api client done

---
+++ client/apiClient.js
@@ -0,0 +1,14 @@
+const request = require('superagent')
+const url = '/api/v1/cars/'
+
+export function getAllCars() {
+  return request
+    .get(url)
+    .then((response) => {
+      const cars = response.body
+      return cars
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
 import Car from './Car'
 import Card from './Card'
 import AddCar from './AddCar'
+import * as api from '../apiClient'
 
 function App() {
+  useEffect(() => {
+    api
+      .getAllCars()
+      .then((carData) => {
+        console.log(carData)
+        return null
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  })
+
   return (
     <>
       <Header />
