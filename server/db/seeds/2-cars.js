7836718a9c9450a8b8b5053d693a541276dc2b07
mike-symon <103223585+mike-symon@users.noreply.github.com>
2022-07-01 15:04:37 +1200

    finished css

--- server/db/seeds/2-cars.js
+++ server/db/seeds/2-cars.js
@@ -10,7 +10,7 @@ exports.seed = function (knex) {
       model_name: 'Tesla Cybertruck',
       model_image:
         'https://i.etsystatic.com/21588727/r/il/f89fab/3386207956/il_1588xN.3386207956_i9mm.jpg',
-      series_id: 2,
+      series_id: 1,
       year: 2024,
     },
     {
@@ -18,7 +18,7 @@ exports.seed = function (knex) {
       model_name: 'Porsche 911',
       model_image:
         'https://i0.wp.com/lamleygroup.com/wp-content/uploads/2014/02/7b576-img_9905.jpg?ssl=1',
-      series_id: 2,
+      series_id: 1,
       year: 1992,
     },
     {
@@ -26,7 +26,7 @@ exports.seed = function (knex) {
       model_name: 'Quick Speed Racer',
       model_image:
         'https://s3-us-west-2.amazonaws.com/images.collecthw.com/eb8158ad-e993-41fd-8236-fbc74a89f5e1_large.png',
-      series_id: 2,
+      series_id: 1,
       year: 1998,
     },
     {
@@ -39,11 +39,11 @@ exports.seed = function (knex) {
     },
     {
       id: 5,
-      model_name: 'Holden Commodore',
+      model_name: 'Toyota Chaser',
       model_image:
-        'https://www.farmers.co.nz/INTERSHOP/static/WFS/Farmers-Shop-Site/-/Farmers-Shop/en_NZ/product/63/31/608/1/6331608_00_W900_H1173.jpg',
+        'https://trademe.tmcdn.co.nz/photoserver/plus/1754860508.jpg',
       series_id: 1,
-      year: 2021,
+      year: 1996,
     },
   ])
 }
